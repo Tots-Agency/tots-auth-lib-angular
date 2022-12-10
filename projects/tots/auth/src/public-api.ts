72bb9c34b87bc19ac43e95ee400b7eab51790a65
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-10 01:50:24 -0300

    Add public guards and interceptor.

--- projects/tots/auth/src/public-api.ts
+++ projects/tots/auth/src/public-api.ts
@@ -9,6 +9,16 @@ export * from './lib/entities/tots-user';
 export * from './lib/entities/tots-token-user';
 
 /**
+ * Interceptors
+ */
+export * from './lib/interceptors/tots-auth.interceptor';
+
+/**
+ * Guards
+ */
+export * from './lib/guards/tots-auth.guard';
+
+/**
  * Services
  */
 export * from './lib/services/tots-auth.service';
