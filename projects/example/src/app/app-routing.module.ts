09280eb1ba26823c5c94a05945855d84f18d3ccf
Matias Camiletti <matias.camiletti@gmail.com>
2023-01-24 18:25:34 -0300

    Creat estructure base for example.

--- projects/example/src/app/app-routing.module.ts
+++ projects/example/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginBasicPageComponent, TotsBaseLoginPageConfig } from 'projects/tots/auth-layout/src/public-api';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'login', 
+    component: LoginBasicPageComponent,
+    data: {
+      pathHome: '/dashboard',
+      pathSuccess: '/dashboard',
+    } as TotsBaseLoginPageConfig
+  },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/example/src/app/app.module.ts
+++ projects/example/src/app/app.module.ts
@@ -4,17 +4,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TotsCoreModule, TOTS_CORE_PROVIDER } from '@tots/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TotsAuthInterceptor, TotsAuthModule } from '@tots/auth';
+import { TotsAuthLayoutModule } from 'projects/tots/auth-layout/src/public-api';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    TotsCoreModule,
+    TotsAuthModule,
+    TotsAuthLayoutModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: TOTS_CORE_PROVIDER,
+      useValue: {
+        baseUrl: 'http://0.0.0.0:8080/'
+      }
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TotsAuthInterceptor,
+      multi: true
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
