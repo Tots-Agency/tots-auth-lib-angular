b1d999326d4d43b250449f655a9dad714f37610e
Matias Camiletti <matias.camiletti@gmail.com>
2024-02-04 18:44:41 -0300

    Fix unathorized.

--- projects/tots/auth/src/lib/interceptors/tots-auth.interceptor.ts
+++ projects/tots/auth/src/lib/interceptors/tots-auth.interceptor.ts
@@ -4,9 +4,10 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpResponse
+  HttpResponse,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable, switchMap, tap } from 'rxjs';
+import { Observable, catchError, switchMap, tap } from 'rxjs';
 import { TotsCoreConfig, TOTS_CORE_PROVIDER } from '@tots/core';
 import { TotsAuthService } from '../services/tots-auth.service';
 
@@ -27,10 +28,14 @@ export class TotsAuthInterceptor implements HttpInterceptor {
     .pipe(switchMap(user => next.handle(request.clone({
       setHeaders: { 'Authorization': 'Bearer ' + user.access_token }
     }))))
-    .pipe(tap(event => {
-      if(event instanceof HttpResponse && event.status == 401){
-        this.authService.signOut().subscribe();
-      }
-    }));;
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status == 401) {
+          this.authService.signOut().subscribe();
+        }
+
+        throw error;
+      })
+    );
   }
 }
